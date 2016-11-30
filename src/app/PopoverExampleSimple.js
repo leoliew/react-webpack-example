f73a3c483ed4b79bcd7a452b061ae1459f82b447
Leo <zmliu0077@gmail.com>
2016-11-30 14:33:52 +0800

    finish popover  example

--- src/app/PopoverExampleSimple.js
+++ src/app/PopoverExampleSimple.js
@@ -3,6 +3,7 @@ import RaisedButton from 'material-ui/RaisedButton';
 import Popover from 'material-ui/Popover';
 import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
+import ShowCaseCard from './ShowCaseCard';
 
 export default class PopoverExampleSimple extends React.Component {
 
@@ -31,27 +32,34 @@ export default class PopoverExampleSimple extends React.Component {
   };
 
   render() {
+    var showCase =
+        <div>
+          <RaisedButton
+            onTouchTap={this.handleTouchTap}
+            label="Click me"
+          />
+          <Popover
+            open={this.state.open}
+            anchorEl={this.state.anchorEl}
+            anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+            targetOrigin={{horizontal: 'left', vertical: 'top'}}
+            onRequestClose={this.handleRequestClose}
+          >
+            <Menu>
+              <MenuItem primaryText="Refresh" />
+              <MenuItem primaryText="Help &amp; feedback" />
+              <MenuItem primaryText="Settings" />
+              <MenuItem primaryText="Sign out" />
+            </Menu>
+          </Popover>
+        </div>
+      ;
     return (
-      <div>
-        <RaisedButton
-          onTouchTap={this.handleTouchTap}
-          label="Click me"
-        />
-        <Popover
-          open={this.state.open}
-          anchorEl={this.state.anchorEl}
-          anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
-          targetOrigin={{horizontal: 'left', vertical: 'top'}}
-          onRequestClose={this.handleRequestClose}
-        >
-          <Menu>
-            <MenuItem primaryText="Refresh" />
-            <MenuItem primaryText="Help &amp; feedback" />
-            <MenuItem primaryText="Settings" />
-            <MenuItem primaryText="Sign out" />
-          </Menu>
-        </Popover>
-      </div>
+      <ShowCaseCard
+        title="Simple example"
+        subtitle="A simple example showing a Popover containing a Menu. It can be also closed by clicking away from the Popover."
+        text={showCase}
+      />
     );
   }
 }
