cc9c04f7c6a18e946535355fcde5219652d2de3b
Leo <zmliu0077@gmail.com>
2016-11-30 14:28:49 +0800

    finish menu  example

--- src/app/MenuExampleSimple.js
+++ src/app/MenuExampleSimple.js
@@ -5,31 +5,42 @@ import React from 'react';
 import Paper from 'material-ui/Paper';
 import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
+import ShowCaseCard from './ShowCaseCard';
 
 const style = {
   display: 'inline-block',
   margin: '16px 32px 16px 0',
 };
 
-const MenuExampleSimple = () => (
-  <div>
-    <Paper style={style}>
-      <Menu>
-        <MenuItem primaryText="Maps" />
-        <MenuItem primaryText="Books" />
-        <MenuItem primaryText="Flights" />
-        <MenuItem primaryText="Apps" />
-      </Menu>
-    </Paper>
-    <Paper style={style}>
-      <Menu>
-        <MenuItem primaryText="Refresh" />
-        <MenuItem primaryText="Help &amp; feedback" />
-        <MenuItem primaryText="Settings" />
-        <MenuItem primaryText="Sign out" />
-      </Menu>
-    </Paper>
-  </div>
-);
+export default class MenuExampleSimple extends React.Component {
 
-export default MenuExampleSimple;
+  render() {
+    var showCase =
+        <div>
+          <Paper style={style}>
+            <Menu>
+              <MenuItem primaryText="Maps"/>
+              <MenuItem primaryText="Books"/>
+              <MenuItem primaryText="Flights"/>
+              <MenuItem primaryText="Apps"/>
+            </Menu>
+          </Paper>
+          <Paper style={style}>
+            <Menu>
+              <MenuItem primaryText="Refresh"/>
+              <MenuItem primaryText="Help &amp; feedback"/>
+              <MenuItem primaryText="Settings"/>
+              <MenuItem primaryText="Sign out"/>
+            </Menu>
+          </Paper>
+        </div>
+      ;
+    return (
+      <ShowCaseCard
+        title="Simple examples"
+        subtitle="Two simple examples. The menu widths adjusts to accommodate the content in keyline increments."
+        text={showCase}
+      />
+    )
+  }
+}
