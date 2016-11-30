12ea6bf09f07522ed825b39f2b8b4d75fe6e0e3b
Leo <zmliu0077@gmail.com>
2016-11-30 15:03:35 +0800

    finish tabs   example

--- src/app/TabsExampleSimple.js
+++ src/app/TabsExampleSimple.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Tabs, Tab} from 'material-ui/Tabs';
 import Slider from 'material-ui/Slider';
+import ShowCaseCard from './ShowCaseCard';
 
 const styles = {
   headline: {
@@ -15,41 +16,51 @@ function handleActive(tab) {
   alert(`A tab with this route property ${tab.props['data-route']} was activated.`);
 }
 
-const TabsExampleSimple = () => (
-  <Tabs>
-    <Tab label="Item One" >
-      <div>
-        <h2 style={styles.headline}>Tab One</h2>
-        <p>
-          This is an example tab.
-        </p>
-        <p>
-          You can put any sort of HTML or react component in here. It even keeps the component state!
-        </p>
-        <Slider name="slider0" defaultValue={0.5} />
-      </div>
-    </Tab>
-    <Tab label="Item Two" >
-      <div>
-        <h2 style={styles.headline}>Tab Two</h2>
-        <p>
-          This is another example tab.
-        </p>
-      </div>
-    </Tab>
-    <Tab
-      label="onActive"
-      data-route="/home"
-      onActive={handleActive}
-    >
-      <div>
-        <h2 style={styles.headline}>Tab Three</h2>
-        <p>
-          This is a third example tab.
-        </p>
-      </div>
-    </Tab>
-  </Tabs>
-);
+export default class TabsExampleSimple extends React.Component {
 
-export default TabsExampleSimple;
+  render() {
+    var showCase =
+        <Tabs>
+          <Tab label="Item One">
+            <div>
+              <h2 style={styles.headline}>Tab One</h2>
+              <p>
+                This is an example tab.
+              </p>
+              <p>
+                You can put any sort of HTML or react component in here. It even keeps the component state!
+              </p>
+              <Slider name="slider0" defaultValue={0.5}/>
+            </div>
+          </Tab>
+          <Tab label="Item Two">
+            <div>
+              <h2 style={styles.headline}>Tab Two</h2>
+              <p>
+                This is another example tab.
+              </p>
+            </div>
+          </Tab>
+          <Tab
+            label="onActive"
+            data-route="/home"
+            onActive={handleActive}
+          >
+            <div>
+              <h2 style={styles.headline}>Tab Three</h2>
+              <p>
+                This is a third example tab.
+              </p>
+            </div>
+          </Tab>
+        </Tabs>
+      ;
+    return (
+      <ShowCaseCard
+        title="Simple example"
+        subtitle="A simple example of Tabs. The third tab demonstrates the onActive property of Tab."
+        text={showCase}
+      />
+    )
+  }
+}
