f52ef44f3b83a0cefe231747e8a5666ef8b16b15
Leo <zmliu0077@gmail.com>
2016-11-30 15:10:06 +0800

    finish all components

--- src/app/TimePickerExampleSimple.js
+++ src/app/TimePickerExampleSimple.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import TimePicker from 'material-ui/TimePicker';
+import ShowCaseCard from './ShowCaseCard';
 
-const TimePickerExampleSimple = () => (
-  <div>
-    <TimePicker
-      hintText="12hr Format"
-    />
-    <TimePicker
-      hintText="12hr Format with auto ok"
-      autoOk={true}
-    />
-    <TimePicker
-      format="24hr"
-      hintText="24hr Format"
-    />
-    <TimePicker
-      disabled={true}
-      format="24hr"
-      hintText="Disabled TimePicker"
-    />
-  </div>
-);
-
-export default TimePickerExampleSimple;
+export default class TimePickerExampleSimple extends React.Component {
+  
+  render() {
+    var showCase =
+        <div>
+          <TimePicker
+            hintText="12hr Format"
+          />
+          <TimePicker
+            hintText="12hr Format with auto ok"
+            autoOk={true}
+          />
+          <TimePicker
+            format="24hr"
+            hintText="24hr Format"
+          />
+          <TimePicker
+            disabled={true}
+            format="24hr"
+            hintText="Disabled TimePicker"
+          />
+        </div>
+      ;
+    return (
+      <ShowCaseCard
+        title="Simple examples"
+        subtitle="Time Picker supports 12 hour and 24 hour formats. In 12 hour format the AM and PM indicators toggle the selected time period. You can also disable the Dialog passing true to the disabled property."
+        text={showCase}
+      />
+    )
+  }
+}
--- src/app/ToolbarExamplesSimple.js
+++ src/app/ToolbarExamplesSimple.js
@@ -7,6 +7,7 @@ import MenuItem from 'material-ui/MenuItem';
 import DropDownMenu from 'material-ui/DropDownMenu';
 import RaisedButton from 'material-ui/RaisedButton';
 import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import ShowCaseCard from './ShowCaseCard';
 
 export default class ToolbarExamplesSimple extends React.Component {
 
@@ -20,36 +21,43 @@ export default class ToolbarExamplesSimple extends React.Component {
   handleChange = (event, index, value) => this.setState({value});
 
   render() {
-    return (
-      <Toolbar>
-        <ToolbarGroup firstChild={true}>
-          <DropDownMenu value={this.state.value} onChange={this.handleChange}>
-            <MenuItem value={1} primaryText="All Broadcasts" />
-            <MenuItem value={2} primaryText="All Voice" />
-            <MenuItem value={3} primaryText="All Text" />
-            <MenuItem value={4} primaryText="Complete Voice" />
-            <MenuItem value={5} primaryText="Complete Text" />
-            <MenuItem value={6} primaryText="Active Voice" />
-            <MenuItem value={7} primaryText="Active Text" />
-          </DropDownMenu>
-        </ToolbarGroup>
-        <ToolbarGroup>
-          <ToolbarTitle text="Options" />
-          <FontIcon className="muidocs-icon-custom-sort" />
-          <ToolbarSeparator />
-          <RaisedButton label="Create Broadcast" primary={true} />
-          <IconMenu
-            iconButtonElement={
+    var showCase =
+        <Toolbar>
+          <ToolbarGroup firstChild={true}>
+            <DropDownMenu value={this.state.value} onChange={this.handleChange}>
+              <MenuItem value={1} primaryText="All Broadcasts"/>
+              <MenuItem value={2} primaryText="All Voice"/>
+              <MenuItem value={3} primaryText="All Text"/>
+              <MenuItem value={4} primaryText="Complete Voice"/>
+              <MenuItem value={5} primaryText="Complete Text"/>
+              <MenuItem value={6} primaryText="Active Voice"/>
+              <MenuItem value={7} primaryText="Active Text"/>
+            </DropDownMenu>
+          </ToolbarGroup>
+          <ToolbarGroup>
+            <ToolbarTitle text="Options"/>
+            <FontIcon className="muidocs-icon-custom-sort"/>
+            <ToolbarSeparator />
+            <RaisedButton label="Create Broadcast" primary={true}/>
+            <IconMenu
+              iconButtonElement={
               <IconButton touch={true}>
                 <NavigationExpandMoreIcon />
               </IconButton>
             }
-          >
-            <MenuItem primaryText="Download" />
-            <MenuItem primaryText="More Info" />
-          </IconMenu>
-        </ToolbarGroup>
-      </Toolbar>
+            >
+              <MenuItem primaryText="Download"/>
+              <MenuItem primaryText="More Info"/>
+            </IconMenu>
+          </ToolbarGroup>
+        </Toolbar>
+      ;
+    return (
+      <ShowCaseCard
+        title="Example"
+        subtitle="An example Toolbar demonstrating the use of the available sub-components, and including a number of other Material-UI components, such as Drop Down Menu, Font Icon, Icon Menu and Raised Button ."
+        text={showCase}
+      />
     );
   }
 }
