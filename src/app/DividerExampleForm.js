7ea0c89deef95bdae4d3eff72c739350532335f5
Leo <zmliu0077@gmail.com>
2016-11-30 14:17:07 +0800

    finish gird list

--- src/app/DividerExampleForm.js
+++ src/app/DividerExampleForm.js
@@ -34,7 +34,4 @@ export default class DividerExampleForm extends React.Component {
       />
     )
   }
-
-
-
 }
--- src/app/GridListExampleSimple.js
+++ src/app/GridListExampleSimple.js
@@ -3,6 +3,7 @@ import {GridList, GridTile} from 'material-ui/GridList';
 import IconButton from 'material-ui/IconButton';
 import Subheader from 'material-ui/Subheader';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+import ShowCaseCard from './ShowCaseCard';
 
 const styles = {
   root: {
@@ -63,25 +64,35 @@ const tilesData = [
 /**
  * A simple example of a scrollable `GridList` containing a [Subheader](/#/components/subheader).
  */
-const GridListExampleSimple = () => (
-  <div style={styles.root}>
-    <GridList
-      cellHeight={180}
-      style={styles.gridList}
-    >
-      <Subheader>December</Subheader>
-      {tilesData.map((tile) => (
-        <GridTile
-          key={tile.img}
-          title={tile.title}
-          subtitle={<span>by <b>{tile.author}</b></span>}
-          actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
-        >
-          <img src={tile.img} />
-        </GridTile>
-      ))}
-    </GridList>
-  </div>
-);
+export default class GridListExampleSimple extends React.Component {
 
-export default GridListExampleSimple;
+  render() {
+    var showCase =
+        <div style={styles.root}>
+          <GridList
+            cellHeight={180}
+            style={styles.gridList}
+          >
+            <Subheader>December</Subheader>
+            {tilesData.map((tile) => (
+              <GridTile
+                key={tile.img}
+                title={tile.title}
+                subtitle={<span>by <b>{tile.author}</b></span>}
+                actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+              >
+                <img src={tile.img}/>
+              </GridTile>
+            ))}
+          </GridList>
+        </div>
+      ;
+    return (
+      <ShowCaseCard
+        title="Simple example"
+        subtitle="A simple example of a scrollable GridList containing a Subheader."
+        text={showCase}
+      />
+    )
+  }
+}
