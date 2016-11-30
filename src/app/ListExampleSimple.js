9e01982e4605e1b2934370162ee0509c6e148d53
Leo <zmliu0077@gmail.com>
2016-11-30 14:25:04 +0800

    finish list example

--- src/app/ListExampleSimple.js
+++ src/app/ListExampleSimple.js
@@ -6,24 +6,36 @@ import ContentSend from 'material-ui/svg-icons/content/send';
 import ContentDrafts from 'material-ui/svg-icons/content/drafts';
 import Divider from 'material-ui/Divider';
 import ActionInfo from 'material-ui/svg-icons/action/info';
+import ShowCaseCard from './ShowCaseCard';
 
-const ListExampleSimple = () => (
-  <div>
-    <List>
-      <ListItem primaryText="Inbox" leftIcon={<ContentInbox />} />
-      <ListItem primaryText="Starred" leftIcon={<ActionGrade />} />
-      <ListItem primaryText="Sent mail" leftIcon={<ContentSend />} />
-      <ListItem primaryText="Drafts" leftIcon={<ContentDrafts />} />
-      <ListItem primaryText="Inbox" leftIcon={<ContentInbox />} />
-    </List>
-    <Divider />
-    <List>
-      <ListItem primaryText="All mail" rightIcon={<ActionInfo />} />
-      <ListItem primaryText="Trash" rightIcon={<ActionInfo />} />
-      <ListItem primaryText="Spam" rightIcon={<ActionInfo />} />
-      <ListItem primaryText="Follow up" rightIcon={<ActionInfo />} />
-    </List>
-  </div>
-);
+export default class ListExampleSimple extends React.Component {
 
-export default ListExampleSimple;
+  render() {
+    var showCase =
+        <div>
+          <List>
+            <ListItem primaryText="Inbox" leftIcon={<ContentInbox />}/>
+            <ListItem primaryText="Starred" leftIcon={<ActionGrade />}/>
+            <ListItem primaryText="Sent mail" leftIcon={<ContentSend />}/>
+            <ListItem primaryText="Drafts" leftIcon={<ContentDrafts />}/>
+            <ListItem primaryText="Inbox" leftIcon={<ContentInbox />}/>
+          </List>
+          <Divider />
+          <List>
+            <ListItem primaryText="All mail" rightIcon={<ActionInfo />}/>
+            <ListItem primaryText="Trash" rightIcon={<ActionInfo />}/>
+            <ListItem primaryText="Spam" rightIcon={<ActionInfo />}/>
+            <ListItem primaryText="Follow up" rightIcon={<ActionInfo />}/>
+          </List>
+        </div>
+      ;
+    return (
+      <ShowCaseCard
+        title="Simple list"
+        subtitle="A simple List with left and right SVG icons."
+        text={showCase}
+      />
+    )
+  }
+
+}
