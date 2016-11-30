c992ec0c14ad10b2f04db782530a509738739084
Leo <zmliu0077@gmail.com>
2016-11-30 14:31:52 +0800

    finish paper  example

--- src/app/PaperExampleSimple.js
+++ src/app/PaperExampleSimple.js
@@ -3,6 +3,7 @@
  */
 import React from 'react';
 import Paper from 'material-ui/Paper';
+import ShowCaseCard from './ShowCaseCard';
 
 const style = {
   height: 100,
@@ -12,14 +13,24 @@ const style = {
   display: 'inline-block',
 };
 
-const PaperExampleSimple = () => (
-  <div>
-    <Paper style={style} zDepth={1} />
-    <Paper style={style} zDepth={2} />
-    <Paper style={style} zDepth={3} />
-    <Paper style={style} zDepth={4} />
-    <Paper style={style} zDepth={5} />
-  </div>
-);
+export default class PaperExampleSimple extends React.Component {
 
-export default PaperExampleSimple;
+  render() {
+    var showCase =
+        <div>
+          <Paper style={style} zDepth={1}/>
+          <Paper style={style} zDepth={2}/>
+          <Paper style={style} zDepth={3}/>
+          <Paper style={style} zDepth={4}/>
+          <Paper style={style} zDepth={5}/>
+        </div>
+      ;
+    return (
+      <ShowCaseCard
+        title="Simple examples"
+        subtitle="Two simple examples. The menu widths adjusts to accommodate the content in keyline increments."
+        text={showCase}
+      />
+    )
+  }
+}
